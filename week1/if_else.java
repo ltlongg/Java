a171d178586566ab7126b5a2b3909475ea1db9eb
ltlongg <160729435+ltlongg@users.noreply.github.com>
2024-02-29 17:46:13 +0700

    code week1

---
+++ week1/if_else.java
@@ -0,0 +1,21 @@
+package javaa;
+public class if_else{
+    public static int hi(int testVal, int target) {
+        int result = 0;
+        if (testVal > target)
+        result = 1;
+        else if (testVal < target)
+        result = -1;
+        else {
+        System.out.println("They are equal");
+        result = 0;
+        }
+        return result;
+    }
+    public static void main(String[] args) {
+        int testVal = 5;
+        int target = 10;
+        int result = hi(testVal, target);
+        System.out.println("Kết quả: " + result);
+    }
+}
