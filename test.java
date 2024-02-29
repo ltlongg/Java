2de2908d512af71c2ceec02f9539f38e163d88ea
ltlongg <160729435+ltlongg@users.noreply.github.com>
2024-02-29 15:07:01 +0700

    Delete test.java

--- test.java
+++
@@ -1,9 +0,0 @@
-public class test{
-  public static void main (String[] args){
-    Integer n1 = Integer.valueOf(47);
-    Integer n2 = Integer.valueOf(50);
-    System.out.println(n1==n2);
-    System.out.println(n1!=n2);
-    System.out.println(n1.equals(n2));
-  }
-}
