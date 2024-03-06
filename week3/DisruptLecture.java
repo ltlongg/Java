09754e93f9176f1c0f6717c830e95258b6524646
ltlongg <160729435+ltlongg@users.noreply.github.com>
2024-03-06 18:02:16 +0700

    Add files via upload

---
+++ week3/DisruptLecture.java
@@ -0,0 +1,24 @@
+package javaa;
+
+class CellPhone {
+    CellPhone(){ }
+    public void ring(Tune t) { 
+        t.play(); 
+    }
+}
+class Tune {
+    public void play() {
+       System.out.println("Tune.play()");
+    }
+}
+class ObnoxiousTune extends Tune{
+    ObnoxiousTune() { }
+    // …
+}
+public class DisruptLecture {
+    public static void main(String[] args) {
+    CellPhone noiseMaker = new CellPhone();
+    ObnoxiousTune ot = new ObnoxiousTune();
+    noiseMaker.ring(ot); // ot works though Tune called for
+    }
+}
